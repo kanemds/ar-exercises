7de89331ad8d9c23a3e0976d2c64db5f71d7cdc1
Kanem <kanemdeng@email.com>
2022-07-03 22:03:01 -0700

    done excercise1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# newer version use ActiveRecord::Base instead of ApplicationRecord
+# creating class Store to reference the database setup
+# class name is singular  
+# database name is plural
+# class name first letter capital
+# database no need
+class Store < ActiveRecord::Base
+
+end 
+
+class Employee < ActiveRecord::Base
+  
+end 
+        
+burnaby = Store.create(name: "burnaby", annual_revenue: 300000, mens_apparel:true, womens_apparel:true)
+richmond = Store.create(name:"richmond",annual_revenue: 1260000, womens_apparel:true)
+gastown = Store.create(name:"gastown",annual_revenue: 190000, mens_apparel:true)
+
+# prints 3 total in Store
+puts Store.count
+
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,17 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+p Store.count
+p Store.all
+
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+
+p@store1
+p@store2
+
+@store1.update(name:"vancouver")
+
+p @store1
